ed0cd4bb822d1105e69a8057c4612ae4c35ad36c
Basilio <basilio.david.g@gmail.com>
2022-04-13 12:19:03 +0200

    feat(delete): delete autogenerated code

--- apps/api/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Message } from '@tfg-daw-basilio/api-interfaces';
-
-@Injectable()
-export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
-}
