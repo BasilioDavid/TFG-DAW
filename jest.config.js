e6e234a3c11dbdbdb7397bf4af18243a0be933ec
Basilio <basilio.david.g@gmail.com>
2022-04-13 14:38:47 +0200

    build(app-controller): add nx test and jest test to packcage json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
+  testMatch: ["**/+.spec.+(.ts)"],
+  testPathIgnorePatterns:["/node_modules/", "/tmp/", "/dist", "/tools/"],
   projects: getJestProjects(),
 };
