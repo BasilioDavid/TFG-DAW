90b65fbca4e224e8fcc9f7ab321bedbf0395363c
Basilio <basilio.david.g@gmail.com>
2022-04-13 20:43:44 +0200

    feat(frontend): add scaffolding

---
+++ apps/tfg-daw-basilio/src/app/pages/file-explorer/file-explorer.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileExplorerComponent } from './file-explorer.component';
+
+@NgModule({
+  declarations: [FileExplorerComponent],
+  imports: [CommonModule],
+})
+export class FileExplorerModule {}
