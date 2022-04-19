70a07ad56e803dbc57369df351cc37ddd7e94f7b
Basilio <basilio.david.g@gmail.com>
2022-04-19 20:45:26 +0200

    feat(front): add some methods WIP

--- apps/api/src/app/core/file-manager/file-manager.controller.ts
+++ apps/api/src/app/core/file-manager/file-manager.controller.ts
@@ -1,18 +1,33 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Response } from 'express';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { FileSystemService } from '../../shared/file-system/file-system.service';
 
 @Controller('file-manager')
 export class FileManagerController {
   public constructor(private readonly fileSystem: FileSystemService) {}
 
-  @Get('ls/:path')
-  public ls(@Param("path") path: string) {
-    console.log(path)
+  @Get('ls')
+  public ls(@Query('path') path: string) {
     return this.fileSystem.ls(path);
   }
 
-  @Get('mkdir/:path/:dirName')
-  public mkdir(@Param("path") path: string, @Param("dirName") dirName: string) {
-    return this.fileSystem.mkdir(path, dirName);
+  @Get('mkdir')
+  public async mkdir(
+    @Res() expressResponse: Response,
+    @Query('path') path: string,
+    @Query('dirName') dirName: string
+  ) {
+    const isOk = await this.fileSystem.mkdir(path, dirName);
+    expressResponse.sendStatus(isOk ? 201 : 406);
+  }
+
+  @Get('touch')
+  public async touch(
+    @Res() expressResponse: Response,
+    @Query('path') path: string,
+    @Query('dirName') dirName: string
+  ) {
+    // const isOk = await this.fileSystem.touch();
+    // expressResponse.sendStatus(isOk ? 201 : 406);
   }
 }
--- apps/api/src/app/shared/file-system/file-system.service.ts
+++ apps/api/src/app/shared/file-system/file-system.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { readdir, stat, mkdir } from 'fs/promises';
-import {ENV} from "../enviroment/env";
+import { ENV } from '../enviroment/env';
 
 @Injectable()
 export class FileSystemService {
@@ -15,17 +15,37 @@ export class FileSystemService {
     try {
       const elements = await readdir(`${this.env.PUBLIC_PATH}/${path}`);
       for (const element of elements) {
-        const isFile = (await stat(`${this.env.PUBLIC_PATH}/${element}`)).isFile();
+        const isFile = (
+          await stat(`${this.env.PUBLIC_PATH}/${path}/${element}`)
+        ).isFile();
         list[isFile ? 'files' : 'directories'].push(element);
       }
     } catch (e) {
       //TODO: change this into a logger service
-      console.log(e)
+      console.log(e);
     }
     return list;
   }
 
+  async touch(path: string, dirName: string) {
+    try {
+      await `${this.env.PUBLIC_PATH}/${path}/${dirName}`;
+      return true;
+    } catch (e) {
+      //TODO: change this into a logger service
+      console.log(e);
+      return false;
+    }
+  }
+
   async mkdir(path: string, dirName: string) {
-    await mkdir(`${this.env.PUBLIC_PATH}/${path}/${dirName}`);
+    try {
+      await mkdir(`${this.env.PUBLIC_PATH}/${path}/${dirName}`);
+      return true;
+    } catch (e) {
+      //TODO: change this into a logger service
+      console.log(e);
+      return false;
+    }
   }
 }
