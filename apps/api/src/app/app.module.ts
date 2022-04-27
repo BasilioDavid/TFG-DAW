ce6bab90e5a5bdd42dd368b96915653b3ec3deb9
Basilio <basilio.david.g@gmail.com>
2022-04-27 21:21:13 +0200

    feat(backend): add environment variables

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
-import {FileManagerController} from "./core/file-manager/file-manager.controller";
-import {FileSystemService} from "./shared/file-system/file-system.service";
-import {env} from "./shared/enviroment/enviroment";
-import {ENV} from "./shared/enviroment/env";
+import { FileManagerController } from './core/modules/file-manager/file-manager.controller';
+import { FileSystemService } from './common/file-system/file-system.service';
+import { env } from './common/enviroment/enviroment';
+import { ENV } from './common/enviroment/env';
 
 @Module({
   imports: [],
   controllers: [FileManagerController],
-  providers: [FileSystemService,{
-    provide: ENV,
-    useValue: env,
-  }],
+  providers: [
+    FileSystemService,
+    {
+      provide: ENV,
+      useValue: env,
+    },
+  ],
 })
 export class AppModule {}
